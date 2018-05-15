327b2136ecd3d032cb432e900b164e7f2bd933f2
ayeV <36230640+ayeV@users.noreply.github.com>
2018-05-15 17:22:14 -0300

    Add files via upload

--- valdez_Ayelen_1A/funciones.h
+++ valdez_Ayelen_1A/funciones.h
@@ -31,6 +31,21 @@ typedef struct
  *
  */
 int menu();
+/**
+ *  Obtiene la opcion del menu elegida por el usuario
+ * \return la opcion elegida por el usuario
+ *
+ */
+int menuModificarProd();
+
+/**
+ *  Obtiene la opcion del menu elegida por el usuario
+ * \return la opcion elegida por el usuario
+ *
+ */
+
+int menuInformar();
+
 
 /** Inicializa el vector prod en 0 para poder hacer una carga aleatoria
  *
@@ -52,21 +67,140 @@ void hardCode(eProveedor proveedores[]);
  * @return el primer indice disponible
  */
 int buscarLibreProd(eProducto prod[],int tam);
+/** Lista los productos segun descripcion (ascendente)  e importe (descendente)
+ *
+ * \param prod el array se pasa por parametro
+ * \param tam tamanio del array
+ */
+
 void listarProductos(eProducto prod[],int tamProd);
+/**
+ * Obtiene el indice que coincide con el ID pasado por parametro.
+ * @param prov el array se pasa como parametro.
+* \param tam tamanio del array
+ * @param ID el ID a ser buscado en el array.
+ * @return el indice en donde se encuentra el elemento que coincide con el parametro id
+ */
 int buscarProv(eProveedor prov[],int tam, int id);
+/** Muestra la cantidad de productos con stock mayor a 10
+ *
+ * \param prod el array se pasa por parametro
+ * \param tam tamanio del array
+*/
+
 void cantProdConStockMayorADiez(eProducto prod[], int tam);
+/** Muestra la cantidad de productos con stock menor a 10
+ *
+ * \param prod el array se pasa por parametro
+ * \param tam tamanio del array
+*/
 void cantProdConStockMenorADiez(eProducto prod[], int tam);
 
+/** Muestra los productos con stock mayor a 10
+ *
+ * \param prod el array se pasa por parametro
+ * \param tam tamanio del array
+*/
+
+void listarProdCantMayorADiez(eProducto prod[],int tam);
+/** Muestra los productos con stock menor a 10
+ *
+ * \param prod el array se pasa por parametro
+ * \param tam tamanio del array
+*/
+void listarProdCantMenorADiez(eProducto prod[],int tam);
+
+
+
+/** Muestra un solo producto
+ *
+ * \param prod el array se pasa por parametro
+*/
+
+
 void mostrarProd(eProducto prod);
+/** Muestra todos los producto
+ *
+ * \param vec el array se pasa por parametro
+ * \param tam tamanio del array vec
+*/
+
+
+void mostrarProds(eProducto vec[], int tam);
+
+/** Agrega un producto
+ *
+ * \param producto array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ * \param prov array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ */
+
+
+
 void altaProd(eProducto producto[],int tamProd, eProveedor prov[],int tamProv);
 
+/** Muestra los productos de un proveedor determinado
+ *
+ * \param prov el array se pasa por parametro
+ * \param prod el array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ * \param tamProd tamanio del array prod
+*/
+
+
 void mostrarProveedorDeterminadoYProductos(eProveedor prov[],eProducto prod[], int tamProv, int tamProd);
+/** Muestra un submenu con las opciones
+ *
+ * \param prod el array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ * \param prov el array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ */
+
 
 void listar(eProducto prod[],int tamProd, eProveedor prov[], int tamProv);
+/** Muestra un submenu con las opciones
+ *
+ * \param prod el array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ */
 void informar(eProducto prod[],int tamProd);
+
+/** Lista los prod del proveedor que provee mas productos
+ *
+ * \param prod el array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ * \param prov el array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ */
+
 void listar9(eProducto prod[],int tamProd,eProveedor prov[],int tamProv);
+/** Lista los prod del proveedor que provee menos productos
+ *
+ * \param prod el array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ * \param prov el array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ */
+
 void listar10(eProducto prod[],int tamProd,eProveedor prov[],int tamProv);
+/** Muestra el prod mas caro que provee un proveedor
+ *
+ * \param prod el array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ * \param prov el array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ */
+
 void listar11(eProducto prod[],int tamProd, eProveedor prov[],int tamProv);
+/** Muestra el prod mas barato que provee un proveedor
+ *
+ * \param prod el array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ * \param prov el array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ */
 void listar12(eProducto prod[],int tamProd, eProveedor prov[],int tamProv);
 
 
@@ -79,14 +213,27 @@ void listar12(eProducto prod[],int tamProd, eProveedor prov[],int tamProv);
  */
 
 int buscarProd(eProducto prod[],int tam, int id);
+
+/** Muestra todos los proveedores con sus productos
+ *
+ * \param prov el array se pasa por parametro
+ * \param prod el array se pasa por parametro
+ * \param tamProv tamanio del array prov
+ * \param tamProd tamanio del array prod
+ */
+
 void mostrarProveedorYProductos(eProveedor prov[],eProducto productos[], int tamProv, int tamProd);
 
-/**
- *  Obtiene la opcion del menu elegida por el usuario
- * \return la opcion elegida por el usuario
+
+
+/** Modifica los campos de un producto
  *
+ * \param prod el array se pasa por parametro
+ * \param tamProd tamanio del array prod
+ * \param prov el array se pasa por parametro
+ * \param tamProv tamanio del array prov
  */
-int menuModificarProd();
+
 void modificaProd(eProducto prod[], int tamProd, eProveedor prov[],int tamProv);
 
 /**
